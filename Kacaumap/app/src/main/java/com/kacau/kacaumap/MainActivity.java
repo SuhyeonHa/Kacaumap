c5cb9b1da2730396a3db7183f14155dbec1d66c7
sojeongyim <ssoww7474@naver.com>
2018-04-09 23:55:49 +0900

    marker

--- Kacaumap/app/src/main/java/com/kacau/kacaumap/MainActivity.java
+++ Kacaumap/app/src/main/java/com/kacau/kacaumap/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kacau.kacaumap;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -11,15 +13,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.skt.Tmap.TMapData;
 import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPOIItem;
 import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapTapi;
 import com.skt.Tmap.TMapView;
 
 import java.util.ArrayList;
+import java.util.logging.LogManager;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -37,6 +48,22 @@ public class MainActivity extends AppCompatActivity
         contentTmap.addView( tMapView );
         tMapView.setCenterPoint(126.956895, 37.504242);
         tMapView.setZoomLevel(17);
+        tMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+        //지도
+
+        Button searchbtn = (Button) findViewById(R.id.searchbtn);
+        searchbtn.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, Search.class);
+                        EditText editSearch1 = (EditText)findViewById(R.id.editSearch1);
+                        String keyword=editSearch1.getText().toString();
+                        intent.putExtra("editSearch1", keyword);
+                        startActivity(intent);
+                    }
+                }
+        );
+        //버튼누르면 값전달과함께 화면전환
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
--- Kacaumap/app/src/main/java/com/kacau/kacaumap/Search.java
+++ Kacaumap/app/src/main/java/com/kacau/kacaumap/Search.java
@@ -7,7 +7,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -19,11 +21,15 @@ public class Search extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
         ArrayAdapter adapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1,LIST_MENU);
-
         ListView listview=(ListView)findViewById(R.id.listview2);
         listview.setAdapter(adapter);
+        Intent intent =getIntent();
+
+        EditText editSearch=(EditText)findViewById(R.id.editSearch);
+        String keyword= intent.getStringExtra("editSearch1");
+
+        editSearch.setText(keyword); //임시로 값띄워 값전달확인
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
@@ -31,7 +37,11 @@ public class Search extends AppCompatActivity {
                 String item = String.valueOf(parent.getItemAtPosition(position));
                 Toast.makeText(Search.this, item, Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(Search.this, ResultMap.class);
+                String[] POI={"37.503667","126.957139","5층"};//나중에 받을 list의 위치정보
+                intent.putExtra("searchPOI", POI);
                 startActivity(intent);
+
+
             }});
     }
 }
